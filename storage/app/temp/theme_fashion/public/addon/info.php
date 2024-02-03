04211a27c0134234811b922dccffb22a9d7b50de
Hafiz Hanif <hafiz.hanif992@gmail.com>
2024-02-03 12:18:24 +0500

    init

---
+++ storage/app/temp/theme_fashion/public/addon/info.php
@@ -0,0 +1,8 @@
+<?php return array (
+  'software_id' => '0001',
+  'name' => 'Lifestyle Theme',
+  'is_active' => 0,
+  'purchase_code' => '',
+  'username' => '',
+  'image' => 'fashion_theme.png',
+);
