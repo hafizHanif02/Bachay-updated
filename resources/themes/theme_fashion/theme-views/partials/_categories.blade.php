a495bf6d086ed0b356a0d13a4f5ecc382c8cf707
Ali Azeem Khan <azeem.alikhan777@gmail.com>
2024-03-07 16:57:35 +0500

    Update _categories.blade.php

--- resources/themes/theme_fashion/theme-views/partials/_categories.blade.php
+++ resources/themes/theme_fashion/theme-views/partials/_categories.blade.php
@@ -11,22 +11,6 @@
         <div class="most-visited-category-wrapper align-items-center row">
             @foreach ($most_visited_categories as $key => $item)
                 {{-- @if ($key != 0 && $key < 8) --}}
-                @if($item->id != '1')
-                    <div class="col-6 col-sm-4 col-md-3 col-lg-3">
-                        <a href="{{ route('products', ['id' => $item->id, 'data_from' => 'category', 'page' => 1]) }}"
-                            class="most-visited-item d-block text-center mb-4">
-                            <img loading="lazy" alt="{{ translate('category') }}"
-                                src="{{ getValidImage(path: 'storage/app/public/category/' . $item->icon, type: 'category') }}"
-                                class="img-fluid mb-2">
-                            <h4 class="title">{{ $item->name }}</h4>
-                            <div class="cont">
-                                <h6 class="text-white font-semibold text-uppercase">{{ $item->name }}</h6>
-                                <span>{{ $item->product_count }} {{ translate('product') }}</span>
-                                <i class="bi bi-eye-fill"></i>
-                            </div>
-                        </a>
-                    </div>
-                    @elseif($item->id == '1')
                     <div class="col-6 col-sm-4 col-md-3 col-lg-3">
                         <a href="{{ route('categories.detail', $item->id) }}"
                             class="most-visited-item d-block text-center mb-4">
@@ -41,7 +25,6 @@
                             </div>
                         </a>
                     </div>
-                    @endif
                 {{-- @endif --}}
             @endforeach
         </div>
