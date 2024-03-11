1d1cd0e7a09cb72edd9c24c0b2dbd70fe8fb29f9
Hassan-Abdullah5 <145267400+Hassan-Abdullah5@users.noreply.github.com>
2024-03-11 17:28:16 +0500

    Update custom_page.blade.php

--- resources/themes/theme_fashion/theme-views/custom_page/custom_page.blade.php
+++ resources/themes/theme_fashion/theme-views/custom_page/custom_page.blade.php
@@ -20,18 +20,18 @@
         display: flex;
         flex-wrap: wrap;
     }
-
 </style>
 
 @section('content')
-<div class="custom-main-con">
-    @foreach ($custom_page->page_data as $page_data)
-        <div class="custom-page-banner" style="width: {{ $page_data->width }}%; margin-right: {{ $page_data->margin_right }}px; margin-bottom: {{ $page_data->margin_bottom }}px;">
-            <a href="">
-                <img src="{{ asset('public/assets/images/customePage/' . $custom_page->id . '/' . $custom_page->resource_id . '/' . $page_data->image) }}"
-                    alt="" width="100%">
-            </a>
-        </div>
-    @endforeach
-</div>
+    <div class="custom-main-con">
+        @foreach ($custom_page->page_data as $page_data)
+            <div class="custom-page-banner"
+                style="width: {{ $page_data->width }}%; margin-right: {{ $page_data->margin_right }}px; margin-bottom: {{ $page_data->margin_bottom }}px;">
+                <a href="{{ $page_data->url }}">
+                    <img src="{{ asset('public/assets/images/customePage/' . $custom_page->id . '/' . $custom_page->resource_id . '/' . $page_data->image) }}"
+                        alt="" width="100%">
+                </a>
+            </div>
+        @endforeach
+    </div>
 @endsection
