7305a10de25a642df6e05c8bd364adbecbbdfed7
Ali Azeem Khan <azeem.alikhan777@gmail.com>
2024-03-14 13:16:47 +0500

    child

--- resources/themes/theme_fashion/theme-views/partials/_add-child-mobile.blade.php
+++ resources/themes/theme_fashion/theme-views/partials/_add-child-mobile.blade.php
@@ -36,38 +36,96 @@
         margin: 0;
     }
 </style>
-<div class="show-div d-md-none d-lg-none d-xl-none" id="showDiv">
+<div class="show-div d-lg-none d-xl-none" id="showDiv">
     <span class="close-btn-add" onclick="hideDiv()"><i class="bi bi-x-lg"></i></span>
     <div id="slider_part">
+        <a href="{{ route('my-child.list') }}">
         <div class="slider_child">
             <img alt="Add Child" class="static_img gastaticimage" src="{{ asset('public/images/plus-blue.png') }}"
                 width="50px">
             <p class="R11_42">Add Child</p>
         </div>
+        </a>
         
         <div class="slider_child">
             <img alt="Shop for Kids" title="Shop for Kids" class="static_img gastaticimage"
                 src="{{ asset('public/images/all_p_icon.png') }}" width="50px">
-            <p class="R11_42">All</p>
+            {{-- <p >All</p> --}}
+            <form action="{{ route('unswitch') }}" method="get">
+                @csrf
+                <button type="submit" class="R11_42">All</button>
+            </form>
         </div>
 
+        @auth('customer')
+        <?php $childs = \App\Models\FamilyRelation::where('user_id', Auth::guard('customer')->user()->id)->get();
+        ?>
+        @if(!$childs->isEmpty())
+            @foreach($childs as $child)
+            <div class="slider_child">
+                <img alt="boy" class="static_img gastaticimage"
+                src="{{asset('public/assets/images/customers/child/'.$child->profile_picture)}}" width="50px">
+                <p class="R11_42">
+                    <form action="{{ route('switch_child', $child->id) }}" method="get">
+                        @csrf
+                    <button class="btn btn-{{ $child->gender == 'male' ? 'primary' : 'custom-pink' }}">{{ $child->name }}</button>
+                    </form>
+                </p>
+            </div>
+            @endforeach
+        @else
         <div class="slider_child">
             <img alt="boy" class="static_img gastaticimage"
                 src="{{ asset('public/images/boy_p_icon.png') }}" width="50px">
-         
-            <p class="R11_42">Boy</p>
+            <form action="{{ route('switch_male') }}" method="get">
+                @csrf
+                <button type="submit" class="btn btn-primary"><p class="R11_42">Boy</p></button>
+            </form>
         </div>
         <div class="slider_child">
             <img alt="girl" class="static_img gastaticimage"
                 src="{{ asset('public/images/girl_p_icon.png') }}" width="50px">
      
-            <p class="R11_42">Girl</p>
+            <p class="R11_42">
+                <form action="{{ route('switch_female') }}" method="get">
+                    @csrf
+                    <button type="submit" class="btn btn-custom-pink"><p class="R11_42">Girl</p></button>
+                </form>
+            </p>
         </div>
         <div class="slider_child">
             <img alt="expecting" class="static_img gastaticimage"
                 src="{{ asset('public/images/add-child1.png') }}" width="50px">
             <p class="R11_42">Expecting</p>
         </div>
+        @endif
+        @endauth
+        @guest('customer')
+        <div class="slider_child">
+            <img alt="boy" class="static_img gastaticimage"
+                src="{{ asset('public/images/boy_p_icon.png') }}" width="50px">
+            <form action="{{ route('switch_male') }}" method="get">
+                @csrf
+                <button type="submit" class="btn btn-primary"><p class="R11_42">Boy</p></button>
+            </form>
+        </div>
+        <div class="slider_child">
+            <img alt="girl" class="static_img gastaticimage"
+                src="{{ asset('public/images/girl_p_icon.png') }}" width="50px">
+     
+            <p class="R11_42">
+                <form action="{{ route('switch_female') }}" method="get">
+                    @csrf
+                    <button type="submit" class="btn btn-custom-pink"><p class="R11_42">Girl</p></button>
+                </form>
+            </p>
+        </div>
+        <div class="slider_child">
+            <img alt="expecting" class="static_img gastaticimage"
+                src="{{ asset('public/images/add-child1.png') }}" width="50px">
+            <p class="R11_42">Expecting</p>
+        </div>
+        @endguest
     </div>
 </div>
 <script>
