e724027f1551a752df2cf05a8b1aed51e6c5a025
Hassan-Abdullah5 <145267400+Hassan-Abdullah5@users.noreply.github.com>
2024-03-29 10:38:39 +0500

    coming soon

--- resources/themes/theme_fashion/theme-views/coming-soon/coming-soon.blade.php
+++ resources/themes/theme_fashion/theme-views/coming-soon/coming-soon.blade.php
@@ -1,36 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-   <style>
-    .coming-soon img{
-      width: 500px;
-      
-    }
-    body{
-      background: #B6E0F6;
-    }
-    .coming-soon{
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-    @media (max-width: 768px){
-      .coming-soon img{
-      width: 100%;
-      
-    }
-    }
-   </style>
-  </head>
-  <body>
-   <div class="coming-soon">
 
-     <img src="{{ asset('public/images/coming-soon1.jpg') }}" alt="">
-   </div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coming Soon</title>
+    <style>
+        * {
+            padding: 0;
+            margin: 0;
+        }
+
+        body {
+            background: #B6E0F6;
+        }
+
+        img {
+            max-width: 100%;
+            height: auto;
+        }
+
+        .mobile-image {
+            display: none;
+        }
+
+        .image-container {
+            text-align: center;
+        }
+
+        @media screen and (max-width: 767px) {
+            .image-container {
+                text-align: center;
+            }
+
+            .desktop-image {
+                display: none;
+            }
+
+            .mobile-image {
+                display: block;
+            }
+        }
+    </style>
+</head>
+
+<body>
    
-  </body>
+    <div class="image-container desktop-image">
+        <img src="{{ asset('public/images/fordesktop.jpg') }}" alt="Desktop Image">
+    </div>
+
+   
+    <div class="image-container mobile-image">
+        <img src="{{ asset('public/images/formobile.png') }}" alt="Mobile Image">
+    </div>
+</body>
+
 </html>
- 
