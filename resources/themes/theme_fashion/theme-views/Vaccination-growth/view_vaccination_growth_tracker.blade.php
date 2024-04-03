bab2d013ac5e2f008459506d93daf8ac38ee7a08
Ali Azeem Khan <azeem.alikhan777@gmail.com>
2024-04-03 12:30:55 +0500

    Update view_vaccination_growth_tracker.blade.php

--- resources/themes/theme_fashion/theme-views/Vaccination-growth/view_vaccination_growth_tracker.blade.php
+++ resources/themes/theme_fashion/theme-views/Vaccination-growth/view_vaccination_growth_tracker.blade.php
@@ -7,7 +7,6 @@
     <meta property="og:url" content="{{ config('app.url') }}">
     <meta property="og:description"
         content="{{ substr(strip_tags(str_replace('&nbsp;', ' ', $web_config['about']->value)), 0, 160) }}">
-
     <meta property="twitter:card"
         content="{{ asset('storage/app/public/company') }}/{{ $web_config['web_logo']->value }}" />
     <meta property="twitter:title" content="Welcome To {{ $web_config['name']->value }} Home" />
