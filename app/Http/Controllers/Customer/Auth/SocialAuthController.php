ea04a8b9c4810d7d0e03b2429470f3e0dde62c88
Hafiz Hanif <hafiz.hanif992@gmail.com>
2024-05-08 15:43:29 +0500

    Update SocialAuthController.php

--- app/Http/Controllers/Customer/Auth/SocialAuthController.php
+++ app/Http/Controllers/Customer/Auth/SocialAuthController.php
@@ -23,9 +23,9 @@ class SocialAuthController extends Controller
 
     public function handleProviderCallback($service)
     {
-        return $service;
         $user_data = Socialite::driver($service)->stateless()->user();
-        
+
+        dd($user_data); 
         $user = User::where('email', $user_data->getEmail())->first();
 
         $name = explode(' ', $user_data['name']);
