03267dbe77556948f0ca60ff3c1aeb2b72ff6978
Hafiz Hanif <hafiz.hanif992@gmail.com>
2024-05-18 16:04:03 +0500

    Update home.blade.php

--- resources/themes/theme_fashion/theme-views/home.blade.php
+++ resources/themes/theme_fashion/theme-views/home.blade.php
@@ -96,11 +96,11 @@
     @endif
 
     @if ($promo_banner_bottom)
-        <div class="container">
+        <div class="container mt-2">
             <div class="row">
         @foreach ($promo_banner_bottom as $key => $item)
-            <div class="col-12 col-sm-6 col-md-4 col-lg-4">
-                <a href="{{ $item->url }}" target="_blank" class="d-block promotional-banner">
+            <div class="col-12 col-sm-6 col-md-4 col-lg-4" style="padding-top: 0.5rem; padding-bottom: 0.5rem">
+                <a href="{{ $item->url }}" target="_blank" class="d-block promotional-banner" style="border-radius:0.42rem;">
                     <img loading="lazy" class="w-100" alt="{{ translate('banner') }}"
                          src="{{ getValidImage(path: 'storage/app/public/banner/'.$item['photo'], type:'banner') }}">
                 </a>
