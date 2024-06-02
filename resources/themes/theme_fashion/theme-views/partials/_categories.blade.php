40886b8e0171de2a8750182a5cd33e3fb912da0f
Hafiz Hanif <hafiz.hanif992@gmail.com>
2024-06-02 19:17:59 +0500

    home page category css & styling

--- resources/themes/theme_fashion/theme-views/partials/_categories.blade.php
+++ resources/themes/theme_fashion/theme-views/partials/_categories.blade.php
@@ -1,5 +1,5 @@
 <section class="most-visited-category section-gap pb-0 text-center pt-3">
-    <div class="container">
+    <div class="container-fluid">
         <div class="section-title-3 mb-0">
             {{-- <div class="mb-32px"> --}}
             {{-- <h2 class="title mx-auto mb-0 text-capitalize">{{ translate('most_visited_categories') }}</h2> --}}
@@ -13,7 +13,7 @@
                 {{-- @if ($key != 0 && $key < 8) --}}
                     <div class="col-3 col-sm-3 col-md-2 col-lg-2">
                         <a href="{{ route('categories.detail', $item->name) }}"
-                            class="most-visited-item d-block text-center mb-4">
+                            class="most-visited-item d-block text-center ">
                             <img loading="lazy" alt="{{ translate('category') }}"
                                 src="{{ getValidImage(path: 'storage/app/public/category/' . $item->icon, type: 'category') }}"
                                 class="img-fluid mb-2">
