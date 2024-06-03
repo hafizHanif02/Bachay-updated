dfced8bd504e07969dd6d91ecff09abc85c18f5e
Hafiz Hanif <hafiz.hanif992@gmail.com>
2024-06-03 12:44:44 +0500

    home page category styling

--- resources/themes/theme_fashion/theme-views/partials/_categories.blade.php
+++ resources/themes/theme_fashion/theme-views/partials/_categories.blade.php
@@ -76,6 +76,75 @@
             </div>
             
         </div>
+
+        <div class="most-visited-category-wrapper align-items-center row">
+            @foreach ($most_visited_categories as $key => $item)
+                {{-- @if ($key != 0 && $key < 8) --}}
+                    <div class="col">
+                        <a href="{{ route('categories.detail', $item->name) }}"
+                            class="most-visited-item d-block text-center ">
+                            <img loading="lazy" alt="{{ translate('category') }}"
+                                src="{{ getValidImage(path: 'storage/app/public/category/' . $item->icon, type: 'category') }}"
+                                class="img-fluid mb-2">
+                            <h4 class="title">{{ $item->name }}</h4>
+                            <h4 class="title2">Category</h4>
+                            <!-- <div class="cont">
+                                <h6 class="text-white font-semibold text-uppercase">{{ $item->name }}</h6>
+                                <span>{{ $item->product_count }} {{ translate('product') }}</span>
+                                <i class="bi bi-eye-fill"></i>
+                            </div> -->
+                        </a>
+                    </div>
+                {{-- @endif --}}
+            @endforeach
+
+            <div class="col">
+                        <a href="{{ route('categories.detail', $item->name) }}"
+                            class="most-visited-item d-block text-center ">
+                            <img loading="lazy" alt="{{ translate('category') }}"
+                                src="https://bachay.com/public/assets/images/staticImages/girl0to6.png"
+                                class="img-fluid mb-2">
+                            <h4 class="title">0-6 Months</h4>
+                            <h4 class="title2">Girls Fashion</h4>
+                            <!-- <div class="cont">
+                                <h6 class="text-white font-semibold text-uppercase">{{ $item->name }}</h6>
+                                <span>{{ $item->product_count }} {{ translate('product') }}</span>
+                                <i class="bi bi-eye-fill"></i>
+                            </div> -->
+                        </a>
+            </div>
+            <div class="col">
+                        <a href="{{ route('categories.detail', $item->name) }}"
+                            class="most-visited-item d-block text-center ">
+                            <img loading="lazy" alt="{{ translate('category') }}"
+                                src="https://bachay.com/public/assets/images/staticImages/boy0to6.png"
+                                class="img-fluid mb-2">
+                            <h4 class="title">0-6 Months</h4>
+                            <h4 class="title2">Boys Fashion</h4>
+                            <!-- <div class="cont">
+                                <h6 class="text-white font-semibold text-uppercase">{{ $item->name }}</h6>
+                                <span>{{ $item->product_count }} {{ translate('product') }}</span>
+                                <i class="bi bi-eye-fill"></i>
+                            </div> -->
+                        </a>
+            </div>
+            <div class="col">
+                        <a href="{{ route('categories.detail', $item->name) }}"
+                            class="most-visited-item d-block text-center ">
+                            <img loading="lazy" alt="{{ translate('category') }}"
+                                src="https://bachay.com/public/assets/images/staticImages/girl6to24.png"
+                                class="img-fluid mb-2">
+                            <h4 class="title">6-24 Months</h4>
+                            <h4 class="title2">Girls Fashion</h4>
+                            <!-- <div class="cont">
+                                <h6 class="text-white font-semibold text-uppercase">{{ $item->name }}</h6>
+                                <span>{{ $item->product_count }} {{ translate('product') }}</span>
+                                <i class="bi bi-eye-fill"></i>
+                            </div> -->
+                        </a>
+            </div>
+            
+        </div>
         
     </div>
 </section>
