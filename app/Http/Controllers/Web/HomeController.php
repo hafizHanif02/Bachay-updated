afd66b9e431e31bcef4456692a193761db0b49f2
Hafiz Hanif <hafiz.hanif992@gmail.com>
2024-08-24 03:09:29 +0500

    Update HomeController.php

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -392,7 +392,7 @@ class HomeController extends Controller
     public function toys(Request $request){
         $theme_name = theme_root_path();
         $banners = Banner::where(['resource_type'=> 'category', 'published'=> 1, 'resource_id'=>3, 'banner_type'=>'Promo Deal Banner'])->latest()->get();
-        $brandBanner = Banner::where(['resource_type'=> 'brand', 'published'=> 1, 'resource_id'=>3, 'banner_type'=>'Brand Banner'])->latest()->get();
+        $brandBanner = Banner::where(['resource_type'=> 'category', 'published'=> 1, 'resource_id'=>3, 'banner_type'=>'Brand Banner'])->latest()->get();
 
         return view(VIEW_FILE_NAMES['toys'], compact('banners', 'brandBanner'));
     }
